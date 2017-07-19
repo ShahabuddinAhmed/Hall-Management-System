13c1bd2490d7da15d9c609e8bdf97dc622c8e349
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2017-07-20 01:18:06 +0600

    Admin Password is changed

--- AdminChangePassword.php
+++ AdminChangePassword.php
@@ -8,39 +8,39 @@
         <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/style.css">
-    
+
     </head>
 
     <body>
 
         <div class="form">
-            
+
             <div class="tab-content"> 
-              
+
                 <h1>Welcome Back!</h1>
-              
+
                 <form action="/" method="post">
-          
+
                     <div class="field-wrap">
                         <label>
                             Enter New Password<span class="req">*</span>
                         </label>
                         <input type="password"required autocomplete="off"/>
                     </div>
-          
+
                     <div class="field-wrap">
                         <label>
                             Enter Confirm Password<span class="req">*</span>
                         </label>
                         <input type="password"required autocomplete="off"/>
                     </div>
-          
+
                     <button class="button button-block"/>Confirm</button>
-          
+
                 </form>
 
             </div>
-          
+
         </div> <!-- /form -->
 
         <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
