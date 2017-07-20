72f6c6b286945fa1d83489d7757f0d6f1fc0e053
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2017-07-21 00:45:02 +0600

    Session ID is changed

--- Student.php
+++ Student.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
-    if(!isset($_SESSION['ID']) || empty($_SESSION['ID']) || $_SESSION['ID']=="Admin" ){
+    if(!isset($_SESSION['ID']) || empty($_SESSION['ID']))
+    {
         header('location:HallManagementSystem.php'); 
     }
 
