a114b889573aa7dc19171839756e47386e9f5d19
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2017-07-21 14:09:58 +0600

    ID and Room number is checked for valid or invalid

--- Admit.php
+++ Admit.php
@@ -11,48 +11,92 @@
 		$Room=$_POST['room'];
 		$Add=$_POST['addr'];
 		$Mob=$_POST['mobile'];
-		$RoomType=$_POST['roomType'];
+		//$RoomType=$_POST['roomType'];
+
+		include 'dbconnect.php';
+
+		$sql = "SELECT room FROM floor WHERE room = '".$Room."' ";
+		$result = $conn->query($sql);
+		if($result->num_rows > 0)
+		{
+			$sql = "SELECT one, two, three, four FROM floor WHERE room = '".$Room."' ";
+			$result = $conn->query($sql);
+			while($row = $result->fetch_assoc())
+			{
+				$counter = 0;
+				if($row["one"] == 1)
+				{
+					$counter = $counter + 1;
+				}
+				if($row["two"] == 1)
+				{
+					$counter = $counter + 1;
+				}
+				if($row["three"] == 1)
+				{
+					$counter = $counter + 1;
+				}
+				if($row["four"] == 1)
+				{
+					$counter = $counter + 1;
+				}
+
+				if($row["one"] == 0)
+				{
+					$counter = $counter + 1;
+				}
+				if($row["two"] == 0)
+				{
+					$counter = $counter + 1;
+				}
+				if($row["three"] == 0)
+				{
+					$counter = $counter + 1;
+				}
+				if($row["four"] == 0)
+				{
+					$counter = $counter + 1;
+				}
+
+				$RoomType = $counter;
+
+			}
+
+		}
+		else
+		{
+			echo "This Room Number Dose not Exit";
+		}
+
 
 		$birthdate= date("m/d/Y", strtotime($_POST["birth"]));
 		$dateObj = DateTime::createFromFormat('m/d/Y', $birthdate);
 		$birthdate = $dateObj->format('Y/m/d');
 
-		if($Roll!="" && $Namef!="" && $Namel!="" && $Dept!="" && $Year!="" && $Session!="" && $Room!="" && $Add!="" && $Mob!="" && $RoomType!="" && $birthdate!=""){
-			include 'dbconnect.php';
+		if($Roll!="" && $Namef!="" && $Namel!="" && $Dept!="" && $Year!="" && $Session!="" && $Room!="" && $Add!="" && $Mob!="" && $RoomType!="" && $birthdate!="")
+		{
 
-			$sql = "SELECT COUNT(*)as total FROM student WHERE Room=".$Room;
-			$result = $conn->query($sql);
-			if($result->num_rows > 0)
+			$Name=$Namef." ".$Namel;
+
+			$curDate=date("Y-m-d");
+
+			$sql = "INSERT INTO student VALUES('$Roll','$Name','$Dept','$Year','$Session','$Room','$Add','$Mob','$RoomType','$birthdate','$birthdate','$curDate')";
+			if($conn->query($sql)===TRUE)
 			{
-				$row_array=$result->fetch_array();
-				$tm=$row_array['total'];
-				if($tm<$RoomType){
-					$Name=$Namef." ".$Namel;
-
-					$curDate=date("Y-m-d");
-					
-					$sql = "INSERT INTO student VALUES('$Roll','$Name','$Dept','$Year','$Session','$Room','$Add','$Mob','$RoomType','$birthdate','$birthdate','$curDate')";
-					if($conn->query($sql)===TRUE){
-						header('location:Administration.php');
-					}
-					else{
-						echo "Eoor.".$conn->error;
-					}
-				}
-				else{
-					echo "The Room Already Full";
-				}
+				header('location:Administration.php');
 			}
-			else{
-				echo "Error=".$conn->error;
+			else
+			{
+				echo "Eoor.".$conn->error;
 			}
-
+		}
+		else
+		{
+			echo "Please Fill All Info.";
+		}
 	}
-	else{
-		echo "Please Fill All Info.";
-	}
-}
-	else{
+	else
+	{
 		echo "Youu are Not Logged In.";
 	}
 ?>
