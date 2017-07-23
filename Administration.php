1e603b80fbf8b3ccc96f121d75b9f42a24f49545
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2017-07-23 20:34:35 +0600

    View Meal Number Button is added

--- Administration.php
+++ Administration.php
@@ -201,6 +201,7 @@
                                 <li class = "Left2"><a class = "Left3" href="UpdateStudentInformation.php"> Update Student Information </a></li>
                                 <li class = "Left2"><a class = "Left3" href="ViewRoomInfo.php"> View Room Info </a></li>
                                 <li class = "Left2"><a class = "Left3" href="addNewRoomNumber.php"> Add New Room Number </a></li>
+                                <li class = "Left2"><a class = "Left3" href="viewMealNumber.php"> View Meal Number </a></li>
                             </ul>
                         </div>
                     </div>
